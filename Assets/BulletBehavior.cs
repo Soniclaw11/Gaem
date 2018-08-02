a4c2101484b5829a3901049989f8d6962ab25f94
Hyperspace146 <adriandinh146@gmail.com>
2018-08-01 18:01:45 -0700

    Implementing bullet ricochets (in progress)

--- Assets/BulletBehavior.cs
+++ Assets/BulletBehavior.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
-{ 
+{
+    bool hasRebounded;
+
 	// Use this for initialization
 	void Start ()
     {
-        //e
+        hasRebounded = false;
     }
 	
 	// Update is called once per frame
@@ -16,13 +18,30 @@ public class BulletBehavior : MonoBehaviour
 		
 	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
         if (collision.gameObject.tag == "Player")
         {
+            Destroy(gameObject);
             // Add knockback to player in direction bullet was traveling at a force of 20 times the velocity
             collision.gameObject.GetComponent<Rigidbody2D>().AddForce(gameObject.GetComponent<Rigidbody2D>().velocity * 20);
+        }
+        else if (collision.gameObject.tag == "Wall")
+        {
+            if (hasRebounded)  // if already ricocheted once before colliding with this wall, kill bullet
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                // mark that the bullet has now rebounded once
+                hasRebounded = true;
+
+                // move on to calculate ricochet physics
+
+                // get point of contact with wall and bullet
+                ContactPoint2D contact = collision.contacts[0];
+            }
         }
     }
 }
