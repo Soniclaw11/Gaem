1e20aa2a849e4bad29788bd8bad5f0a3e0569982
Hyperspace146 <adriandinh146@gmail.com>
2018-08-12 23:12:37 -0700

    Got crosshairs working (accuracy not implemented yet)
    -Made a crosshair sprite
    -Raycast outward at the distance the crosshair will be at, and place the crosshair at the point where the ray collides into something (or at the end of the ray if it doesn't collide into anything)
    -Put bullets on a different layer so that the crosshair raycast ignores it
    -No prefab used, crosshair gameobject manually created in player controllers

--- Assets/Player2Controller.cs
+++ Assets/Player2Controller.cs
@@ -12,6 +12,10 @@ public class Player2Controller : MonoBehaviour
     private bool canShoot;
     private IEnumerator coroutine;
     private float bulletSpeed;
+    private GameObject crosshair;
+    private Sprite crosshairSprite;
+    private SpriteRenderer crosshairRenderer;
+    public Texture2D crosshairTexture;
 
     // Use this for initialization
     void Start()
@@ -22,6 +26,11 @@ public class Player2Controller : MonoBehaviour
         moveVertical = 0.0f;
         speedValue = 250 * Time.deltaTime;
         rb = gameObject.GetComponent<Rigidbody2D>();
+
+        crosshair = new GameObject("Crosshair");
+        crosshairSprite = Sprite.Create(crosshairTexture, new Rect(0.0f, 0.0f, crosshairTexture.width, crosshairTexture.height), new Vector2(0.5f, 0.5f), 65.0f);
+        crosshairRenderer = crosshair.AddComponent<SpriteRenderer>();
+        crosshairRenderer.sprite = crosshairSprite;
     }
 
     private void FixedUpdate()
@@ -67,6 +76,21 @@ public class Player2Controller : MonoBehaviour
         if (Input.GetKey(KeyCode.N))
             transform.Rotate(Vector3.forward * 4);
 
+        // Crosshair positioning: Raycast ahead the distance the bullet is 100% accurate to the crosshair (see if/else below)
+        int layerMask = 1 << 2;
+        layerMask = ~layerMask;  // Inverts bitmask so that the ray collides against everything not in layer 2 (bullets are in layer 2)
+        RaycastHit2D raycastInfo = Physics2D.Raycast(transform.position + transform.right * 0.2f + transform.up * 0.7f, transform.rotation * Vector2.up, 13.0f, layerMask);
+        if (raycastInfo.collider)  // if the ray hit a collider, place crosshair at the place the ray collided
+        {
+            crosshair.transform.position = raycastInfo.point;
+        }
+        else  //if it didn't, place the crosshair at the end of the ray
+        {
+            Ray2D ray = new Ray2D(transform.position + transform.right * 0.2f + transform.up * 0.7f, transform.rotation * Vector2.up);
+            crosshair.transform.position = ray.GetPoint(13.0f);
+        }
+        crosshair.transform.rotation = transform.rotation;
+
         // Shoot bullets when key pressed. Sets canShoot to false and then starts a one-second timer to set it to true again
         if (Input.GetKeyDown(KeyCode.Comma) && canShoot)
         {
@@ -74,6 +98,7 @@ public class Player2Controller : MonoBehaviour
             // About the bullet position; we change the spawn point of the bullet from the center of the character to the tip of the gun
             bullet = Instantiate(bulletPrefab, transform.position + transform.right * 0.2f + transform.up * 0.7f, transform.rotation);
             bullet.AddForce(transform.rotation * Vector2.up * bulletSpeed * Time.deltaTime);
+            bullet.gameObject.layer = 2;  // move bullet to a separate layer so that the crosshair raycast does not run into it
 
             canShoot = false;
             coroutine = shootTimer(1.0f);
